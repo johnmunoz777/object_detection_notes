beb7513290dbd39ee73094cc6aa48decb629ffc8
shanti <johnmunoz777@outlook.com>
2025-01-12 12:25:37 -0700

    first commit

---
+++ ch_one_yolo/test.py
@@ -0,0 +1,14 @@
+import cv2
+from ultralytics import YOLO
+### This will download the weights
+### Key- we don't want to download the weights every single time
+##  Created a folder to store the weights and call the model on the image
+
+#model=   YOLO('yolov8l.pt')
+#results= model('C:/Users/johnm/OneDrive/youtube_object_det/Images/1.png',show=True)
+#cv2.waitKey(0)
+
+model=  YOLO("./Yolo-Weights/yolov8l.pt")
+results= model("../Images/1.png",show=True)
+cv2.waitKey(0)
+
