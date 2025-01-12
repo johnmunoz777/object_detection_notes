439426801917ae637a0b39d8c6c3021f3c3df5dd
shanti <johnmunoz777@outlook.com>
2025-01-12 14:07:19 -0700

    add more code

--- ch_one_yolo/test.py
+++ ch_one_yolo/test.py
@@ -8,7 +8,14 @@ from ultralytics import YOLO
 #results= model("../Images/1.png",show=True")
 #cv2.waitKey(0)
 
-model=  YOLO("./Yolo-Weights/yolov8l.pt")
-results= model("../Images/1.png",show=True)
-cv2.waitKey(0)
+#model=  YOLO("./Yolo-Weights/yolov8l.pt")
+#results= model("../Images/1.png",show=True)
+#cv2.waitKey(0)
 
+#model=  YOLO("./Yolo-Weights/yolov8l.pt")
+#results= model("../Images/2.png",show=True)
+#cv2.waitKey(0)
+
+model=  YOLO("./Yolo-Weights/yolov8n.pt")
+results= model("../Images/3.png",show=True)
+cv2.waitKey(0)
